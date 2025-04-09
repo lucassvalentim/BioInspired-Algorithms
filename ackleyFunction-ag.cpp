0bc05f88a4f36d34ccada5217eddac3e197ec537
lucassvalentim <lucas.valentimrocha@gmail.com>
2025-04-09 14:47:24 -0300

    new changes

--- ackleyFunction-ag.cpp
+++ ackleyFunction-ag.cpp
@@ -2,85 +2,93 @@
 using std::cout;
 using std::endl;
 
-namespace algorithm
-{
-    namespace detail
-    {   
-        double bitTointeger(std::string bit){
-            double integer = 0;
-            double position = 1;
-            for(int i = bit.size() - 1; i >= 0; i--){
-                if(bit[i] == '1') integer += position;
-                position *= 2;
-            }
+// namespace algorithm
+// {
+//     namespace detail
+//     {   
+//         double bitTointeger(std::string bit){
+//             double integer = 0;
+//             double position = 1;
+//             for(int i = bit.size() - 1; i >= 0; i--){
+//                 if(bit[i] == '1') integer += position;
+//                 position *= 2;
+//             }
 
-            return integer;
-        }
+//             return integer;
+//         }
 
-        auto get_random_bit = []() -> char
-        {
-            const char charset[] = "01";
-            const size_t max_index = (sizeof(charset) - 1);
-            return charset[rand() % max_index];
-        };
+//         auto get_random_bit = []() -> char
+//         {
+//             const char charset[] = "01";
+//             const size_t max_index = (sizeof(charset) - 1);
+//             return charset[rand() % max_index];
+//         };
 
-        auto get_random_string(const size_t length){
-            std::string str(length, 0);
-            std::generate_n(str.begin(), length, get_random_bit);
-            return str;
-        }
+//         auto get_random_string(const size_t length){
+//             std::string str(length, 0);
+//             std::generate_n(str.begin(), length, get_random_bit);
+//             return str;
+//         }
 
-        std::vector<double> decode(std::vector<std::pair<int, int>> &bounds, std::size_t n_bits, std::string bitstring){
-            std::vector<double> decoded(bounds.size());
-            double largest = pow(2, n_bits);
-            for(int i = 0; i < bounds.size(); i++){ 
-                std::string substring = "";
-                int start = i * n_bits;
-                int end = (i * n_bits) + n_bits;
-                for(int j = start; j < end; j++){
-                    substring += bitstring[j];
-                }
-                cout << "substring: " << substring << endl;
+//         std::vector<double> decode(std::vector<std::pair<int, int>> &bounds, std::size_t n_bits, std::string bitstring){
+//             std::vector<double> decoded(bounds.size());
+//             double largest = pow(2, n_bits);
+//             for(int i = 0; i < bounds.size(); i++){ 
+//                 std::string substring = "";
+//                 int start = i * n_bits;
+//                 int end = (i * n_bits) + n_bits;
+//                 for(int j = start; j < end; j++){
+//                     substring += bitstring[j];
+//                 }
+//                 cout << "substring: " << substring << endl;
 
-                double integer = bitTointeger(substring);
-                cout << "largest: " << largest << endl;
-                cout << "integer: " << integer << endl;
-                cout << "inter/largest: " << (integer/largest) << endl;
-                double value = bounds[i].first + (integer/largest) * (bounds[i].second - bounds[i].first);
+//                 double integer = bitTointeger(substring);
+//                 cout << "largest: " << largest << endl;
+//                 cout << "integer: " << integer << endl;
+//                 cout << "inter/largest: " << (integer/largest) << endl;
+//                 double value = bounds[i].first + (integer/largest) * (bounds[i].second - bounds[i].first);
 
-                decoded[i] = value;
-            }
+//                 decoded[i] = value;
+//             }
 
-            return decoded;
-        }
-    } // namespace detail
+//             return decoded;
+//         }
+//     } // namespace detail
 
-    class individual {
-        private:
-            void calculate_fitness(std::vector<int> x){
-
-            }
-    }
+//     class individual {
+//         private:
+//             void calculate_fitness(std::vector<int>& x){
+//                 // m_fitness = -20*exp();
+//             }
+        
+//         private:
+//             std::string m_value;
+//             std::size_t m_fitness;
+//     }
     
-}
+// }
 
 int main(){
 
-    srand(time(NULL));
+    // srand(time(NULL));
 
-    int n_bits = 128;
-    std::vector<std::pair<int, int>> bounds = {{-2, 2}, {-2, 2}, {-2, 2}};
-    std::string s = algorithm::detail::get_random_string(n_bits*bounds.size());
-    cout << "s: " << s << endl;
-    std::vector<double> values = algorithm::detail::decode(bounds, n_bits, s); 
+    // int n_bits = 128;
+    // std::vector<std::pair<int, int>> bounds = {{-2, 2}, {-2, 2}, {-2, 2}};
+    // std::string s = algorithm::detail::get_random_string(n_bits*bounds.size());
+    // cout << "s: " << s << endl;
+    // std::vector<double> values = algorithm::detail::decode(bounds, n_bits, s); 
     
     
-    cout << s << endl;
-    cout << algorithm::detail::bitTointeger(s) << endl;
-    for(auto v : values){
-        cout << v << ' ';
+    // cout << s << endl;
+    // cout << algorithm::detail::bitTointeger(s) << endl;
+    // for(auto v : values){
+    //     cout << v << ' ';
+    // }
+    // cout << endl;
+    
+    void calculate_fitness(std::vector<int>& x){
+        int tam = x.size();
     }
-    cout << endl;
-    
+        
     return 0;
 }
